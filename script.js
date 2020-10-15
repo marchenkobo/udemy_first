b401f71e6c8ca84301337d82388789f667ea811d
Bohdan <bohdanmarchenko@gmail.com>
2020-10-15 19:43:59 +0300

    Условия

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 "use strict";
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?',0);
-console.log(numberOfFilms);
+const num = 30;
 
-const personaMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-};
-
+if (num > 50){
+    console.log('Больше 50-ти');
+} else if (num < 10){
+    console.log('Больше 10-ти');
+} else{
+    console.log(`Равно ${num}-ти`);
+}
 
 
-personaMovieDB.movies[prompt('Один из последних просмотренных фильмов?')] = +prompt('На сколько оценете его?', 0);
-personaMovieDB.movies[prompt('Один из последних просмотренных фильмов?')] = +prompt('На сколько оценете его?', 0);
-console.log(personaMovieDB.movies);
+num > 10 ? console.log("Больше 10-ти") : console.log("Меньше 10-ти");
+
+switch(num + 1){
+    case num < 10:
+        console.log("Меньше 10");
+        break;
+    case num > 60:
+        console.log("Больше 60-ти");
+        break;
+    default:
+        console.log("Между 10 и 60");
+        // если default последний или гарантированно совпадёт с как-то вариантом выше *break можно не использовать
+}
