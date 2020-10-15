e1c40cb5af112fb0cb5ccb1e97a43750a12ee43b
Bohdan <bohdanmarchenko@gmail.com>
2020-10-15 12:42:08 +0300

    Pactice part 1

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 "use strict";
 
-let a;
-let b;
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?',0);
+console.log(numberOfFilms);
+
+const personaMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+
+
+personaMovieDB.movies[prompt('Один из последних просмотренных фильмов?')] = +prompt('На сколько оценете его?', 0);
+personaMovieDB.movies[prompt('Один из последних просмотренных фильмов?')] = +prompt('На сколько оценете его?', 0);
+console.log(personaMovieDB.movies);
