853bda1d84deda95a1ab8937a5ca88b6332aaede
Bohdan <bohdanmarchenko@gmail.com>
2020-10-22 12:31:43 +0300

    Практика (часть 2)

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
 "use strict";
 
-let num = 10;
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?',0);
 
-while(num > 8){
-    console.log(num + ' while');
-    num--;
+const personaMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
 };
 
+// for(let i = 0; i < 3; i++){
+//     let movieTitle;
+//     let movieRate;
 
-do{
-    console.log(num + ' do/while'); //Код выполняется до проверки условия
-    num++;
-} while (num < 11);
+//     do{
+//         movieTitle = prompt("Один из последних просмотренных фильмов?");
+//     } 
+//     while(movieTitle === null || movieTitle.length == 0 || movieTitle.length > 50);
+    
+//     do{
+//         movieRate = +prompt("На сколько оценете его?",0);
+//     } 
+//     while(isNaN(movieRate));
+    
+//     personaMovieDB.movies[movieTitle] = movieRate;
+// }   
 
 
-for (let i = 0; i < 10; i++){
-    if(i === 6){
-        break;  // break - завершает выполнения цикла при заданном условии
+for(let i = 0; i < 2; i++){
+    let movieTitle = prompt("Один из последних просмотренных фильмов?");
+    let movieRate = +prompt("На сколько оценете его?", 0);
+    if(movieTitle != null && movieTitle != '' && !isNaN(movieRate && movieRate != '')){
+        personaMovieDB.movies[movieTitle] = movieRate; 
+        console.log("Done");
+    } else{
+        console.log("Error");
+        i--;
     }
-    if( i === 3){
-        continue; // continue - позволяет "пропустить шаг" при заданном условии
-    }
-    console.log(i + ' for');
 }
+
+if(personaMovieDB.count < 10){
+    console.log("Просмотрено мало фильмов");
+} else if(personaMovieDB.count >= 10 && personaMovieDB.count < 30){
+    console.log("Вы классический зритель");
+} else if(personaMovieDB.count >= 30){
+    console.log("Вы Киноман");
+} else{
+    console.log("Произошла ошибка");
+}
+
+
+console.log(personaMovieDB.movies);
