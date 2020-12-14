a4fb91413650270cd3e3eb7b0cabde51c92e144a
marchenkobo <bohdanmarchenko@gmail.com>
2020-12-14 15:48:49 +0200

    Передача по ссылке или по значению, Spread оператор (ES6-ES9)

--- script.js
+++ script.js
@@ -1,13 +1,59 @@
 "use strict";
 
-let goods = 'milk, bread, meat';
+let obj = {
+    a: 0,
+    b: 1,
+    c: 2,
+    d: {
+        e: 5,
+        f: 6
+    }
+};
 
-let goodsArr = goods.split(', '); // создать массив из строки (указав разделитель)
-console.log(goodsArr);
-console.log(goodsArr.join('; ')); // создать строку из массива (с розделителем)
+// Создание (поверхностной) копии объекта через цикл:
+let objCopy = {};
+for(let prop in obj){
+    objCopy[prop] = obj[prop];
+}
 
-let arr = [1,4,6,8,9];
+objCopy.d.e = 5.5; // !!! Меняет и вложеный (глубокий) объект объекта "obj"
+objCopy.g = 7; // Изменение только в скопированном объекте
 
-arr.forEach(function(item, counter, arr){
-    console.log(`In position ${counter} we can see item: ${item} wich placed in ${arr} massive`);
-});
+console.log(obj);
+console.log(objCopy);
+
+
+// Создание (поверхностной) копии объекта через обьъеденение объектов:
+let objClone = Object.assign({}, obj);
+console.log(objClone);
+
+objClone.d.e = 5.6; // Изменение вложенног объекта также редактируют оригинал
+objClone.j = 8; 
+console.log(obj);
+console.log(objClone);
+
+
+// Создание копии массива (!помиомо цикла)
+let arr = ['a', 'b', 'c'];
+
+let newArr = arr.slice();
+newArr.push('d');
+console.log(arr);
+console.log(newArr);
+
+
+// Spread оператор (*оператор разворота) позволяет "разобрать" массив на отдельные элементы 
+let video = ['yourube', 'vimeo', 'coub'],
+    socials = ['vk', 'facebook', 'instagram'],
+    internet = [...video, ...socials, 'telegram', 'viber'];
+
+console.log(internet);
+
+let arrayClone = [...arr]; //Создание копии массива с помощью spread оператора
+arrayClone.push('newValue')
+console.log(arrayClone);
+
+
+//Копирование объекта с помощью Spread оператора
+let newObj = {...obj}; 
+console.log(newObj);
