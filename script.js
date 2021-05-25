235f986596fae54e04df0f185a97ce4fcf60bb6e
Bohdan Marchenko <bohdanmarchenko@gmail.com>
2021-05-25 22:12:13 +0300

    Скрипты и время их выполнения. setTimeout и setInterval

--- script.js
+++ script.js
@@ -1,58 +1,30 @@
 "use strict";
 
-const btns = document.querySelectorAll('button'),
-    wrapper = document.querySelector('.container');
+let timerId,
+    i = 0;
 
-// btns[0].classList.add('new');
-// btns[0].classList.remove('one');
-// btns[0].classList.toggle('some');
-
-// if(btns[1].classList.contains('second')){
-//     btns[1].classList.add('hand');
-// } else{
-//     btns[1].classList.add('just-second'); 
-// }
-
-// btns[1].addEventListener('click', (e) => {
-//     btns[0].classList.toggle('red');
-// });
-
-
-// wrapper.addEventListener('click', (e) => {
-//     console.dir(e.target);
-//     if (e.target && e.target.className == 'red'){
-//         alert("Попавсь!");
-//     }
-// });
-
-wrapper.addEventListener('click', (e) => {
-    console.dir(e.target);
-    if (e.target && e.target.classList.contains('one')){
-        alert("Нашел!");
+    window.addEventListener('click', (e) => {
+        timerId = setInterval(logger, 500);
+    });
+    function logger(){
+        if (i === 2){
+            //сброс интервала
+            clearInterval(timerId);
+        }
+        console.log('Click detected');
+        i++;
     }
-});
 
-
-//Вешаем обработчик на родителя и ловим элемент, по которму происходил клик
-wrapper.addEventListener('click', (e) => {
-    console.dir(e.target);
-    if (e.target && e.target.tagName == 'BUTTON'){
-        alert("Попавсь!");
-    }
-});
-
-
-
-//Отработчик будет работать даже с динамически добавленным элементом
-const newButton = document.createElement('button');
-newButton.textContent = btns.length + 1;
-wrapper.append(newButton);
-
-
-//Рекомендация Google
-wrapper.addEventListener('click', (e) => {
-    console.dir(e.target);
-    if (e.target && e.target.matches('button.google')){
-        alert("Я через знакомство");
-    }
-});
+    // Рекурсивный setTimeout (замена setInterval`у)
+    let b = 0;
+    let ownSetInterval = setTimeout(function log(){
+        console.log(b);
+        if (b < 2){
+            ownSetInterval = setTimeout(log, 500);
+            b++;
+        } 
+        else{
+            console.log('End');
+            console.log(log);
+        }
+    }, 500);
